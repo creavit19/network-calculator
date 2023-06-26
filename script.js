948e852920872e2dc3507a02c36131ae3b70c668
Vladimir Ilinskiy <creavit@i.ua>
2023-06-26 16:12:46 +0300

    fourth commit

--- script.js
+++ script.js
@@ -8,69 +8,104 @@ var dt = {
         outAddrTo: document.getElementById("outAddrTo"),
         okt: [],
     },
+    init: function () {
+        for (let i = 0; i <= 3; i++) {
+            dt.els.okt[i] = document.getElementById("ip" + String(i));
+            dt.els.okt[i].oldValue = dt.els.okt[i].value;
+            dt.els.okt[i].onkeydown = function (e) {
+                const n = i;
+                dt.fns.checkP(dt.els.okt[n], e);
+                dt.render();
+            }
+            dt.els.okt[i].oninput = function () {
+                const n = i;
+                dt.fns.check(dt.els.okt[n], 255);
+                dt.render();
+            }
+            dt.els.okt[i].onchange = function () {
+                const n = i;
+                dt.fns.checkF(dt.els.okt[n]);
+                dt.render();
+            }
+        }
+        dt.els.inpNetSize.onkeydown = function (e) {
+            dt.fns.checkP(dt.els.inpNetSize, e);
+            dt.render();
+        }
+        dt.els.inpNetSize.oninput = function () {
+            dt.fns.check(dt.els.inpNetSize, 32);
+            dt.render();
+        }
+        dt.els.inpNetSize.onchange = function () {
+            dt.fns.checkF(dt.els.inpNetSize);
+            dt.render();
+        }
+        dt.els.inpNetSize.oldValue = dt.els.inpNetSize.value;
+        dt.render();
+    },
     fns: {
         countHosts: function () {
             return Math.pow(2, 32 - Number(dt.els.inpNetSize.value));
         },
         oktsToNum: function () {
-            return    Number(dt.els.okt[3].value) * 16777216
-                    + Number(dt.els.okt[2].value) * 65536
-                    + Number(dt.els.okt[1].value) * 256
-                    + Number(dt.els.okt[0].value);
+            return Number(dt.els.okt[3].value) * 16777216
+            + Number(dt.els.okt[2].value) * 65536
+            + Number(dt.els.okt[1].value) * 256
+            + Number(dt.els.okt[0].value);
         },
         numToOktArr: function (numAddr) {
             const result = [];
-            for ( let i = 3 ; i >= 0 ; i-- ) {
+            for (let i = 3; i >= 0; i--) {
                 const k = Math.pow(2, 8 * i);
-                result[i] = parseInt( numAddr / k );
+                result[i] = parseInt(numAddr / k);
                 numAddr -= result[i] * k;
             }
             return result;
         },
         numAddrToOut: function (numAddr) {
             const oktArr = dt.fns.numToOktArr(numAddr);
-            for ( let i = 3 ; i >= 0 ; i-- ) {
+            for (let i = 3; i >= 0; i--) {
                 dt.els.okt[i].value = dt.els.okt[i].oldValue = oktArr[i];
             }
         },
         numAddrToStr: function (numAddr) {
             const oktArr = dt.fns.numToOktArr(numAddr);
             let result = "";
-            for ( let i = 3 ; i >= 0 ; i-- ) {
-                result += String(oktArr[i]) + (i == 0 ? "" : ".");
+            for (let i = 3; i >= 0; i--) {
+                result += String(oktArr[i]) + (i == 0 ? "": ".");
             }
             return result;
         },
         range: function () {
             const a = dt.fns.oktsToNum();
             const c = dt.fns.countHosts();
-            const n = c * parseInt( a / c );
+            const n = c * parseInt(a / c);
             return {
-                min: dt.fns.numAddrToStr( n ),
-                max: dt.fns.numAddrToStr( n + c - 1 ),
+                min: dt.fns.numAddrToStr(n),
+                max: dt.fns.numAddrToStr(n + c - 1),
             };
         },
         netMask: function () {
-            const netSize = Number( dt.els.inpNetSize.value );
+            const netSize = Number(dt.els.inpNetSize.value);
             let numAddr = 0;
-            for ( let i = 31 ; i >= (32 - netSize) ; i-- ) {
+            for (let i = 31; i >= (32 - netSize); i--) {
                 numAddr += Math.pow(2, i);
             }
-            return dt.fns.numAddrToStr( numAddr );
+            return dt.fns.numAddrToStr(numAddr);
         },
         checkP: function (fl, e) {
-            if ( !((e.keyCode >= 48 && e.keyCode <= 57)
-                || (e.keyCode >= 37 && e.keyCode <= 40)
-                || (e.keyCode == 8)
-                || (e.keyCode == 9)
-                || (e.keyCode == 13)
-                || (e.keyCode == 229)
-                ) ) {
+            if (!((e.keyCode >= 48 && e.keyCode <= 57)
+            || (e.keyCode >= 37 && e.keyCode <= 40)
+            || (e.keyCode == 8)
+            || (e.keyCode == 9)
+            || (e.keyCode == 13)
+            || (e.keyCode == 229)
+            )) {
                 e.preventDefault();
             }
         },
         check: function (fl, max) {
-            if ( !(fl.value === "") ) {
+            if (!(fl.value === "")) {
                 fl.value = Number(fl.value);
                 if (isNaN(fl.value)) {
                     fl.value = fl.oldValue;
@@ -82,7 +117,7 @@ var dt = {
             fl.oldValue = fl.value;
         },
         checkF: function (fl) {
-            if ( fl.value == "" ) {
+            if (fl.value == "") {
                 fl.oldValue = fl.value = 0;
             }
         },
@@ -104,14 +139,14 @@ var dt = {
         },
         incIP: function () {
             const n = dt.fns.oktsToNum() + dt.fns.countHosts();
-            if ( n > 4294967295 ) return;
-            dt.fns.numAddrToOut( n );
+            if (n > 4294967295) return;
+            dt.fns.numAddrToOut(n);
             dt.render();
         },
         decIP: function () {
             const n = dt.fns.oktsToNum() - dt.fns.countHosts();
-            if ( n < 0 ) return;
-            dt.fns.numAddrToOut( n );
+            if (n < 0) return;
+            dt.fns.numAddrToOut(n);
             dt.render();
         },
     },
@@ -123,36 +158,5 @@ var dt = {
         dt.els.outAddrTo.innerText = dt.fns.range().max;
     },
 }
-for ( let i = 0; i <= 3; i++ ) {
-    dt.els.okt[i] = document.getElementById("ip" + String(i));
-    dt.els.okt[i].oldValue = dt.els.okt[i].value;
-    dt.els.okt[i].onkeydown = function (e) {
-        const n = i;
-        dt.fns.checkP(dt.els.okt[n], e);
-        dt.render();
-    }
-    dt.els.okt[i].oninput = function () {
-        const n = i;
-        dt.fns.check(dt.els.okt[n], 255);
-        dt.render();
-    }
-    dt.els.okt[i].onchange = function () {
-        const n = i;
-        dt.fns.checkF(dt.els.okt[n]);
-        dt.render();
-    }
-}
-dt.els.inpNetSize.onkeydown = function (e) {
-    dt.fns.checkP(dt.els.inpNetSize, e);
-    dt.render();
-}
-dt.els.inpNetSize.oninput = function () {
-    dt.fns.check(dt.els.inpNetSize, 32);
-    dt.render();
-}
-dt.els.inpNetSize.onchange = function () {
-    dt.fns.checkF(dt.els.inpNetSize);
-    dt.render();
-}
-dt.els.inpNetSize.oldValue = dt.els.inpNetSize.value;
-dt.render();
+
+dt.init();
